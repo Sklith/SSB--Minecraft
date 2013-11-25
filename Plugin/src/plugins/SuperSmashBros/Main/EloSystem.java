5b82b3b5b365088e5be78946e242fefb3f922093
Gigabyte_Giant <brynden.bielefeld@hotmail.com>
2013-11-24 18:27:32 -0600

    Created EloSystem class.

---
+++ Plugin/src/plugins/SuperSmashBros/Main/EloSystem.java
@@ -0,0 +1,24 @@
+package plugins.SuperSmashBros.Main;
+
+public class EloSystem {
+
+	/* 
+	 * Main class for the IES (Internal Elo System) 
+	 * 
+	 * Explanation on Elo Rating System Located Here:
+	 * 
+	 * https://en.wikipedia.org/wiki/Elo_rating_system
+	 * 
+	 */
+	
+	private final SuperSmashBros plugin;
+	
+	public EloSystem(SuperSmashBros plugin) {
+		
+		this.plugin = plugin;
+		
+	}
+	
+	
+	
+}
