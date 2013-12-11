8e4b3d235f1d3c6dabb393717b3e6b67224cc024
Incomprehendable <2HeartsRemaining@gmail.com>
2013-12-11 15:36:39 -0700

    Fixed up the public arena method.

--- Plugin/src/plugins/SuperSmashBros/Main/Arena.java
+++ Plugin/src/plugins/SuperSmashBros/Main/Arena.java
@@ -26,9 +26,29 @@ public class Arena {
 	int afkKickXp = 5;
 	
 	
-	public Arena(Location loc, int id){
-		this.lobbyspawn = loc;
+	public Arena(Location lobbyspawn, int id, int winXp, int killXp, int afkKickXp, List<String> players, List<Location> startLocations, List<String> queuedPlayers,
+			boolean itemDrop, boolean restoreArena, boolean isRanked, boolean earnXp, boolean enableParticleEffects, boolean enableKnockoutSounds, 
+			boolean useTagAPI, boolean scoreboards, boolean doubleJump){
+		this.lobbyspawn = lobbyspawn;
 		this.id = id;
+		this.winXp = winXp;
+		this.killXp = killXp;
+		this.afkKickXp = afkKickXp;
+		this.players = players;
+		this.startLocations = startLocations;
+		this.queuedPlayers = queuedPlayers;
+		this.itemDrop = itemDrop;
+		this.restoreArena = restoreArena;
+		this.isRanked = isRanked;
+		this.earnXp = earnXp;
+		this.enableParticleEffects = enableParticleEffects;
+		this.enableKnockoutSounds = enableKnockoutSounds;
+		this.useTagAPI = useTagAPI;
+		this.scoreboards = scoreboards;
+		this.doubleJump = doubleJump;
+		this.winXp = winXp;
+		this.killXp = killXp;
+		this.afkKickXp = afkKickXp;
 	}
 	public int getId(){
 		return this.id;
