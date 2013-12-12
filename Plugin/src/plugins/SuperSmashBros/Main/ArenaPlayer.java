dd0bae8d3a6b5bcbe6e9752ff0f20defeb119e98
Incomprehendable <2HeartsRemaining@gmail.com>
2013-12-11 17:41:30 -0700

    Created player object

---
+++ Plugin/src/plugins/SuperSmashBros/Main/ArenaPlayer.java
@@ -0,0 +1,65 @@
+package plugins.SuperSmashBros.Main;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ArenaPlayer {
+	private Player ply;
+	private ItemStack[] storedInv;
+	private ItemStack[] storedArmor;
+	private int elo = 1000;
+	private int food = 20;
+	private double health = 20.0;
+	private float saturation = 15;
+	private Arena arena = null;
+	private GameMode inGameMode = GameMode.ADVENTURE;
+	private int deaths = 0;
+	private Location lastLoc = null;
+	
+	
+	
+	public ArenaPlayer(Player player){
+		this.ply = player;
+	}
+	public Player getPlayer(){
+		return this.ply;
+	}
+	public String sendMessage(String msg){
+		return msg;
+	}
+	public String getName(){
+		return ply.getName();
+	}
+	public int getElo(){
+		return this.elo;
+	}
+	public int getFood(){
+		return this.food;
+	}
+	public float getSaturation(){
+		return this.saturation;
+	}
+	public double getHealth(){
+		return this.health;
+	}
+	public int getDeaths(){
+		return this.deaths;
+	}
+	public Arena getArena(){
+		return this.arena;
+	}
+	public ItemStack[] getStoredInv(){
+		return this.storedInv;
+	}
+	public ItemStack[] getStoredArmor(){
+		return this.storedArmor;
+	}
+	public GameMode getGameMode(){
+		return this.inGameMode;
+	}
+	public Location getLastLocation(){
+		return this.lastLoc;
+	}
+}
