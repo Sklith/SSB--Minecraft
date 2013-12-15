d7bddc85a004296c0ab5fc493f271a5f9378a291
Lukas McDiarmid <lukasmcd.14@gmail.com>
2013-12-15 15:52:41 +1100

    Added Updater and regListeners()

--- Plugin/src/plugins/SuperSmashBros/Main/SSB.java
+++ Plugin/src/plugins/SuperSmashBros/Main/SSB.java
@@ -7,6 +7,11 @@ import org.bukkit.Server;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import plugins.SuperSmashBros.Main.Utils.Updater;
+import plugins.SuperSmashBros.Main.Utils.Updater.UpdateResult;
+import plugins.SuperSmashBros.Main.Utils.Updater.UpdateType;
+import plugins.SuperSmashBros.Main.listeners.OnSignChange;
+
 public class SSB extends JavaPlugin{
 
 	private Server s = Bukkit.getServer();
@@ -14,13 +19,21 @@ public class SSB extends JavaPlugin{
 	private Logger log = s.getLogger();
 	
 	public void onEnable() {
-		
 		log.info("SSB: Minecraft fully enabled!");
 		
+		if(getConfig().getBoolean("Check-For-Updates") == true){
+			Updater updater = new Updater(this, 69480, this.getFile(), UpdateType.DEFAULT, true);
+			if(updater.getResult() == UpdateResult.UPDATE_AVAILABLE){
+				this.getLogger().info("New version available! " + updater.getLatestName());
+			}
+		}		
 	}
 	public void onDisable() {
-		
 		log.info("SSB: Minecraft disabled!");
 		
+	}
+	
+	public void regListeners(){
+		pm.registerEvents(new OnSignChange(), this);
 	}
 }
